fccde611c70156b8937d8b287352772190dc5cd8
mikemskim <mskim@steconsulting.co.kr>
2019-11-20 20:57:59 +0900

    Initial commit

---
+++ app/src/main/java/com/tj/a20191120_02_userlistpractice/BaseActivity.kt
@@ -0,0 +1,9 @@
+package com.tj.a20191120_02_userlistpractice
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+    var mContext = this
+    abstract fun setupEvents()
+    abstract fun setValues()
+}
